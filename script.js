3303b73fbaf46a0d2dd3c00ce0a85909903125bc
casandcasandra <74970582+casandcasandra@users.noreply.github.com>
2021-01-16 20:34:02 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+const grid = document.querySelector(".gridContainer");
+const userInput = document.getElementById("quantity");
+const resetButton = document.querySelector(".reset");
+
+createGrid = () => {
+  for (let i = 0; i < 256; i++) {
+    const div = document.createElement("div"); //create div
+    div.classList.add("square"); //<div class="square">
+    grid.appendChild(div); //add <div class="square"> to gridContainer
+  }
+};
+
+updateGrid = () => {
+  grid.innerHTML = ""; //<div class="gridContainer">""</div>
+  grid.style.setProperty(
+    "grid-template-columns",
+    `repeat(${userInput.value}, 1fr)` //create ${userInput.value} number of columns at 1 fraction
+  );
+  grid.style.setProperty(
+    "grid-template-rows",
+    `repeat(${userInput.value}, 1fr)` //create ${userInput.value} number of rows at 1 fraction
+  );
+  for (let i = 0; i < userInput.value * userInput.value; i++) {
+    const div = document.createElement("div");
+    div.classList.add("square");
+    grid.appendChild(div);
+  }
+
+};
+
+const square = document.querySelector("div");
+square.addEventListener("mouseover", changeColor)
+
+function changeColor(event) {
+    event.target.classList.replace("square", "color");
+}
+
+resetButton.addEventListener("click", updateGrid);
+
+
+createGrid();
